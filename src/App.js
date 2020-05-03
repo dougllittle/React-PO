de74f38c5a915127c20cb9db5a2737372480e559
dougllittle <51254598+dougllittle@users.noreply.github.com>
2020-05-02 23:47:44 -0400

    First Commit
    sup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(event){
+
+    }
+    
+    render() {
+        return (
+          <main className="App">
+            <div id="po-wrap">
+              <div id="po-screen">
+              </div>
+              <div id="po-controls">
+              </div>
+            </div>
+          </main>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './css/Footer.css'
+
+class Footer extends Component{
+	render(){
+		return (
+			<footer className="footer">
+				Sup
+			</footer>
+		)
+	}
+}
+
+export default Footer
